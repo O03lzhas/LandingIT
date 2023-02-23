1e26aff9bf2fb15542dbcea1175265258e9178cc
Olzhas Akzholbay <72250368+O03lzhas@users.noreply.github.com>
2023-02-23 16:41:05 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+let btn = document.querySelector('.price--btn');
+
+
+
+//PopUp
